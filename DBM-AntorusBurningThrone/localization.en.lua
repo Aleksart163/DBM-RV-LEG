17fccebd5340e80150e608e6b2d3c8226b26f1af
Arekusandoru <82957716+Aleksart163@users.noreply.github.com>
2022-03-03 06:06:45 +0400

    fix

---
+++ DBM-AntorusBurningThrone/localization.en.lua
@@ -0,0 +1,209 @@
+if GetLocale() ~= "ruRU" and GetLocale() ~= "ruRU" then return end
+local L
+
+---------------------------
+-- Garothi Worldbreaker --
+---------------------------
+L= DBM:GetModLocalization(1992)
+
+L:SetTimerLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+L:SetMiscLocalization({
+	YellPullGarothi = "Enemy combatants detected. Threat level nominal."
+})
+
+---------------------------
+-- Hounds of Sargeras --
+---------------------------
+L= DBM:GetModLocalization(1987)
+
+L:SetOptionLocalization({
+	SequenceTimers = "Squence the cooldown timers on heroic/mythic difficulty off previous ability casts instead of current ability cast to reduce timer clutter at expense of minor timer accuracy (1-2sec early)"
+})
+
+---------------------------
+-- War Council --
+---------------------------
+L= DBM:GetModLocalization(1997)
+
+L:SetMiscLocalization({
+	YellPullCouncil = "This is one engagement you will not walk away from."
+})
+
+---------------------------
+-- Eonar, the Lifebinder --
+---------------------------
+L= DBM:GetModLocalization(2025)
+
+L:SetTimerLocalization({
+	timerObfuscator		= "Next Obfuscator (%s)",
+	timerDestructor 	= "Next Destructor (%s)",
+	timerPurifier 		= "Next Purifier (%s)",
+	timerBats	 		= "Next Bats (%s)"
+})
+
+L:SetOptionLocalization({
+	timerObfuscator		= DBM_CORE_AUTO_TIMER_OPTIONS["cdcount"]:format("ej16501"),
+	timerDestructor 	= DBM_CORE_AUTO_TIMER_OPTIONS["cdcount"]:format("ej16502"),
+	timerPurifier 		= DBM_CORE_AUTO_TIMER_OPTIONS["cdcount"]:format("ej16500"),
+	timerBats	 		= DBM_CORE_AUTO_TIMER_OPTIONS["cdcount"]:format("ej17039")
+})
+
+L:SetMiscLocalization({
+	YellPullEonar = "Champions! The forces of the Legion seek to capture my essense for their infernal master!",
+	Obfuscators = "Obfuscator",
+	Destructors = "Destructor",
+	Purifiers 	= "Purifier",
+	Bats 		= "Bats",
+	EonarHealth	= "Eonar Health",
+	EonarPower	= "Eonar Power",
+	NextLoc		= "Next:"
+})
+
+---------------------------
+-- Portal Keeper Hasabel --
+---------------------------
+L= DBM:GetModLocalization(1985)
+
+L:SetOptionLocalization({
+	ShowAllPlatforms = "Show all announces regardless of player platform location"
+})
+
+L:SetMiscLocalization({
+	YellPullHasabel = "Is this the best you could muster? Hah. Pathetic.",
+	YellPullHasabel2 = "Your war ends here.",
+	YellPullHasabel3 = "The Legion devastates all who oppose us!",
+	YellPullHasabel4 = "Every world in our path has fallen. Yours is next."
+})
+
+---------------------------
+-- Imonar the Soulhunter --
+---------------------------
+L= DBM:GetModLocalization(2009)
+
+L:SetMiscLocalization({
+	DispelMe = "Dispel Me!"
+})
+
+L:SetMiscLocalization({
+	YellPullImonar = "Your bones will be my biggest payday.",
+	YellPullImonar2 = "Think I'll keep a few of your parts as a trophy.",
+	YellPullImonar3 = "Your heads will adorn my trophy room."
+})
+
+---------------------------
+-- Kin'garoth --
+---------------------------
+L= DBM:GetModLocalization(2004)
+
+L:SetOptionLocalization({
+	InfoFrame =	"Show InfoFrame for fight overview",
+	UseAddTime = "Always show timers for what's coming next when boss leaves initialisation phase instead of hiding them. (If disabled, correct timers will resume when boss becomes active again, but may leave little warning if any cooldowns only had 1-2 seconds left)"
+})
+
+L:SetMiscLocalization({
+	YellPullKingaroth = "Time to work.",
+	YellPullKingaroth2 = "You hope to topple my machines with those pathetic weapons?",
+	YellPullKingaroth3 = "Prepare to be deconstructed."
+})
+
+---------------------------
+-- Varimathras --
+---------------------------
+L= DBM:GetModLocalization(1983)
+
+L:SetMiscLocalization({
+	YellPullVarimathras = "Draw your blades! I will show you torment!",
+	YellPullVarimathras2 = "Come, then. We will trade pain for pain!"
+})
+
+---------------------------
+-- The Coven of Shivarra --
+---------------------------
+L= DBM:GetModLocalization(1986)
+
+L:SetWarningLocalization({
+	Amantul = "Torment of Aman'Thul in 5 sec - switch target",
+	Norgannon = "Torment of Norgannon in 5 sec - run to the center",
+	Golgannet = "Torment of Golganneth in 5 sec - keep a distance of 2m",
+	Kazgagot = "Torment of Khaz'goroth in 5 sec - leave the center"
+})
+
+L:SetTimerLocalization({
+	timerBossIncoming		= DBM_INCOMING
+})
+
+L:SetOptionLocalization({
+	Amantul = "Show special warning 5 seconds before appearing $spell:252479",
+	Norgannon = "Show special warning 5 seconds before appearing $spell:244740",
+	Golgannet = "Show special warning 5 seconds before appearing $spell:244756",
+	Kazgagot = "Show special warning 5 seconds before appearing $spell:244733",
+	timerBossIncoming	= "Show timer for next boss swap",
+	TauntBehavior		= "Set taunt behavior for tank swaps",
+	TwoMythicThreeNon	= "Swap at 2 stacks on mythic, 3 stacks on other difficulties",--Default
+	TwoAlways			= "Always swap at 2 stacks regardless of difficulty",
+	ThreeAlways			= "Always swap at 3 stacks regardless of difficulty",
+	SetLighting			= "Automatically turn lighting setting to low when coven is engaged and restore on combat end (Not supported in mac client since mac client doesn't support low lighting)",
+	InterruptBehavior	= "Set interrupt behavior for raid (Requires raid leader)",
+	Three				= "3 person rotation ",--Default
+	Four				= "4 person rotation ",
+	Five				= "5 person rotation ",
+	IgnoreFirstKick		= "With this option, very first interrupt is excluded in rotation (Requires raid leader)"
+})
+
+L:SetMiscLocalization({
+	YellPullCoven = "I can't wait to hear your flesh sizzle."
+})
+
+---------------------------
+-- Aggramar --
+---------------------------
+L= DBM:GetModLocalization(1984)
+
+L:SetOptionLocalization({
+	ignoreThreeTank	= "Filter Rend/Foe Taunt special warnings when using 3 or more tanks (since DBM can't determine exact tanking rotation in this setup). If any tanks die and it drops to 2, filter auto disables"
+})
+
+L:SetMiscLocalization({
+	YellPullAggramar = "You will burn!",
+	Foe			=	"Foe",
+	Rend		=	"Rend",
+	Tempest 	=	"Tempest",
+	Current		=	"Current:"
+})
+
+---------------------------
+-- Argus the Unmaker --
+---------------------------
+L= DBM:GetModLocalization(2031)
+
+L:SetTimerLocalization({
+	timerSargSentenceCD	= "Sentence CD (%s)"
+})
+
+L:SetOptionLocalization({
+	timerSargSentenceCD = DBM_CORE_AUTO_TIMER_OPTIONS["cdcount"]:format(257966)
+})
+
+L:SetMiscLocalization({
+	YellPullArgus = "Death! Death and pain!",
+	SeaText		=	"{rt6} Haste/Vers",
+	SkyText		=	"{rt5} Crit/Mast",
+	Blight		=	"Blight",
+	Burst		=	"Burst",
+	Sentence	=	"Sentence",
+	Bomb		=	"Bomb"
+})
+
+-------------
+--  Trash  --
+-------------
+L = DBM:GetModLocalization("AntorusTrash")
+
+L:SetGeneralLocalization({
+	name =	"Antorus Trash"
+})
---
+++ DBM-AntorusBurningThrone/localization.ru.lua
@@ -0,0 +1,209 @@
+if GetLocale() ~= "ruRU" then return end
+local L
+--$spell:137162
+--------------------------------
+--Разрушитель миров Кин'гарота--
+--------------------------------
+L= DBM:GetModLocalization(1992)
+
+L:SetTimerLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+L:SetMiscLocalization({
+	YellPullGarothi = "Обнаружен противник. Уровень угрозы - номинальный."
+})
+
+--------------------
+--Гончие Саргераса--
+--------------------
+L= DBM:GetModLocalization(1987)
+
+L:SetOptionLocalization({
+	SequenceTimers =	"Уменьшить кд в таймерах на героической/мифической сложности за счет незначительной точности таймеров (на 1-2 секунды раньше)"
+})
+
+--------------------------
+--Военный совет Анторуса--
+--------------------------
+L= DBM:GetModLocalization(1997)
+
+L:SetMiscLocalization({
+	YellPullCouncil = "От меня ещё никто не уходил живым."
+})
+
+----------------------------------
+--Хранительница порталов Азабель--
+----------------------------------
+L= DBM:GetModLocalization(1985)
+
+L:SetOptionLocalization({
+	ShowAllPlatforms =	"Показать все анонсы независимо от местоположения платформы игрока"
+})
+
+L:SetMiscLocalization({
+	YellPullHasabel = "Ха! Так это и есть лучшие из защитников Азерота?",
+	YellPullHasabel2 = "Ваш поход закончится здесь.",
+	YellPullHasabel3 = "Легион сокрушает всех своих врагов!",
+	YellPullHasabel4 = "Нам покорились все миры. Ваш – следующий."
+})
+
+------------------------------
+--Эонар, Хранительница жизни--
+------------------------------
+L= DBM:GetModLocalization(2025)
+
+L:SetTimerLocalization({
+	timerObfuscator		=	"След. Маскировщик (%s)",
+	timerDestructor 	=	"След. Разрушитель (%s)",
+	timerPurifier 		=	"След. Очиститель (%s)",
+	timerBats	 		=	"След. Летучие мыши (%s)"
+})
+
+L:SetOptionLocalization({
+	timerObfuscator		=	DBM_CORE_AUTO_TIMER_OPTIONS["cdcount"]:format("ej16501"),
+	timerDestructor 	=	DBM_CORE_AUTO_TIMER_OPTIONS["cdcount"]:format("ej16502"),
+	timerPurifier 		=	DBM_CORE_AUTO_TIMER_OPTIONS["cdcount"]:format("ej16500"),
+	timerBats	 		=	DBM_CORE_AUTO_TIMER_OPTIONS["cdcount"]:format("ej17039")
+})
+
+L:SetMiscLocalization({
+	YellPullEonar = "Герои! Демоны пытаются захватить мою сущность для своего повелителя.",
+	Obfuscators =	"Маскировщик",
+	Destructors =	"Разрушитель",
+	Purifiers 	=	"Очиститель",
+	Bats 		=	"Летучие мыши",
+	EonarHealth	= 	"Здоровье Эонар",
+	EonarPower	= 	"Сила Эонар",
+	NextLoc		=	"След:"
+})
+
+--------------------
+--Ловец душ Имонар--
+--------------------
+L= DBM:GetModLocalization(2009)
+
+L:SetMiscLocalization({
+	DispelMe =		"Диспел мне!"
+})
+
+L:SetMiscLocalization({
+	YellPullImonar = "Ваши кости будут щедро оплачены.",
+	YellPullImonar2 = "Я оставлю от вас пару кусочков на память.",
+	YellPullImonar3 = "Ваши головы украсят мою коллекцию."
+})
+
+-------------
+--Кин'гарот--
+-------------
+L= DBM:GetModLocalization(2004)
+
+L:SetOptionLocalization({
+	InfoFrame =	"Показать информационное табло для обзора боя",
+	UseAddTime = "Всегда показывать таймеры, когда босс выходит из фазы инициализации, вместо того, чтобы скрывать их. (Если таймеры отключены, они возобновятся, когда босс снова станет активным, но могут оставить мало предупреждений, если до окончания действия таймеров осталось 1-2 секунды)."
+})
+
+L:SetMiscLocalization({
+	YellPullKingaroth = "За работу!",
+	YellPullKingaroth2 = "И вы надеетесь одолеть мои машины этим жалким оружием?",
+	YellPullKingaroth3 = "Приготовьтесь к утилизации."
+})
+
+--------------
+--Вариматрас--
+--------------
+L= DBM:GetModLocalization(1983)
+
+L:SetMiscLocalization({
+	YellPullVarimathras = "Идите сюда и познайте страдания!",
+	YellPullVarimathras2 = "Нападайте! Я покажу вам, что такое боль!"
+})
+
+----------------
+--Ковен шиварр--
+----------------
+L= DBM:GetModLocalization(1986)
+
+L:SetWarningLocalization({
+	Amantul = "Мучения Аман`тула через 5 сек - переключитесь",
+	Norgannon = "Мучения Норганнона через 5 сек - бегите в центр",
+	Golgannet = "Мучения Голганнета через 5 сек - держите радиус 2м",
+	Kazgagot = "Мучения Каз`горота через 5 сек - уйдите с центра"
+})
+
+L:SetTimerLocalization({
+	timerBossIncoming		= DBM_INCOMING
+})
+
+L:SetOptionLocalization({
+	Amantul = "Спец-предупреждение за 5 сек до появления $spell:252479",
+	Norgannon = "Спец-предупреждение за 5 сек до появления $spell:244733",
+	Golgannet = "Спец-предупреждение за 5 сек до появления $spell:244756",
+	Kazgagot = "Спец-предупреждение за 5 сек до появления $spell:244733",
+	timerBossIncoming	= "Показать таймер для следующей смены босса",
+	TauntBehavior		= "Настройка поведения при смене танка",
+	TwoMythicThreeNon	= "Обмен при 2 стаках на мифик сложности, на 3 стаках в других",--Default
+	TwoAlways			= "Всегда меняться на 2 стаках независимо от сложности",
+	ThreeAlways			= "Всегда меняться на 3 стаках независимо от сложности",
+	SetLighting			= "Автоматическое переключение освещения на низкий уровень, когда ковен задействован и восстановление в конце боя на прежний уровень (не поддерживается в mac-клиенте, т.к. mac-клиент не поддерживает низкое освещение)",
+	InterruptBehavior	= "Настройка поведения прерывания для рейда (требуется права лидера рейда)",
+	Three				= "Чередование 3 человек",--Default
+	Four				= "Чередование 4 человек",
+	Five				= "Чередование 5 человек",
+	IgnoreFirstKick		= "При использовании этой опции первое прерывание исключается из чередования (требуется права лидера рейда)"
+})
+
+L:SetMiscLocalization({
+	YellPullCoven = "Сейчас ваша плоть зашипит на огне."
+})
+
+------------
+--Агграмар--
+------------
+L= DBM:GetModLocalization(1984)
+
+L:SetOptionLocalization({
+	ignoreThreeTank	= "Фильтр специальных предупреждений (Пламя/Сокрушитель) при использовании 3 и более танков (так как DBM не может определить точное чередование танков при таком раскладе). Если танки погибают и количество танков уменьшается до 2, фильтр автоматически отключается."
+})
+
+L:SetMiscLocalization({
+	YellPullAggramar = "Вы сгорите!",
+	Foe			=	"Сокрушитель",
+	Rend		=	"Пламя",
+	Tempest 	=	"Буря",
+	Current		=	"Текущий:"
+})
+
+----------------------
+--Аргус Порабощенный--
+----------------------
+L= DBM:GetModLocalization(2031)
+
+L:SetTimerLocalization({
+	timerSargSentenceCD	= "Восс. Приговор (%s)"
+})
+
+L:SetOptionLocalization({
+	timerSargSentenceCD = DBM_CORE_AUTO_TIMER_OPTIONS["cdcount"]:format(257966)
+})
+
+L:SetMiscLocalization({
+	YellPullArgus = "Смерть! Смерть и боль!",
+	SeaText		=	"{rt6} Хаста/Верса на мне!",
+	SkyText		=	"{rt5} Крит/Маст на мне!",
+	Blight		=	"Чумная сфера на мне!",
+	Burst		=	"Взрыв на мне!",
+	Sentence	=	"Приговор на мне!",
+	Bomb		=	"Бомба на мне!"
+})
+
+-------------
+--Треш-мобы--
+-------------
+L = DBM:GetModLocalization("AntorusTrash")
+
+L:SetGeneralLocalization({
+	name =	"Трэш АПТ"
+})
