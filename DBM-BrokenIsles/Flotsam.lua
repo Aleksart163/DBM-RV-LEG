9b6ec45bb0bc8ef3a03e778e2f1ee61b81e5d59d
Arekusandoru <82957716+Aleksart163@users.noreply.github.com>
2022-03-05 21:19:08 +0400

    fix

--- DBM-BrokenIsles/Flotsam.lua
+++ DBM-BrokenIsles/Flotsam.lua
@@ -15,14 +15,14 @@ mod:RegisterEventsInCombat(
 	"UNIT_SPELLCAST_SUCCEEDED"
 )
 
-local warnYaksam				= mod:NewCastAnnounce(223373, 3)
-local warnJetsam				= mod:NewTargetAnnounce(220295, 2)
+local warnYaksam				= mod:NewCastAnnounce(223373, 3) --Отрыжка
+local warnJetsam				= mod:NewTargetAnnounce(220295, 2) --Мусор
 
-local specWarnGetsam			= mod:NewSpecialWarningDodge(220340, "Tank", nil, nil, 1, 2)
-local specWarnBreakSam			= mod:NewSpecialWarningSpell(223317, "Melee", nil, nil, 1, 2)
+local specWarnGetsam			= mod:NewSpecialWarningDodge(220340, "-Tank", nil, nil, 1, 2) --Мусорка
+local specWarnBreakSam			= mod:NewSpecialWarningSpell(223317, "Melee", nil, nil, 1, 2) --Мусоробой
 
-local timerGetsamCD				= mod:NewCDTimer(53, 220340, nil, nil, nil, 5, nil, DBM_CORE_TANK_ICON)
-local timerYaksamCD				= mod:NewCDTimer(50, 223373, nil, nil, nil, 1)--50-55
+local timerGetsamCD				= mod:NewCDTimer(53, 220340, nil, nil, nil, 5, nil, DBM_CORE_DEADLY_ICON) --Мусорка
+local timerYaksamCD				= mod:NewCDTimer(50, 223373, nil, nil, nil, 1, nil, DBM_CORE_TANK_ICON) --50-55 Отрыжка
 
 --mod:AddReadyCheckOption(37460, false)
 
