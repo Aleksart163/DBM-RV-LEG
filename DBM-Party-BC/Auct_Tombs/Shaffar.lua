aa48244692097c31237c8d37c7ec3f09c4a0e310
Arekusandoru <82957716+Aleksart163@users.noreply.github.com>
2022-03-25 20:01:08 +0400

    fix

---
+++ DBM-Party-BC/Auct_Tombs/Shaffar.lua
@@ -0,0 +1,13 @@
+local mod	= DBM:NewMod(537, "DBM-Party-BC", 4, 250)
+local L		= mod:GetLocalizedStrings()
+
+--mod.statTypes = "normal,heroic,timewalker"
+
+mod:SetRevision(("$Revision: 17650 $"):sub(12, -3))
+mod:SetCreatureID(18344)
+mod:SetEncounterID(1899)
+
+mod:RegisterCombat("combat")
+
+mod:RegisterEventsInCombat(
+)
