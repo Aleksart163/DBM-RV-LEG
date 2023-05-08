f8919f4cd746440ae87afd4393ef1f87e8f33770
Danzoui <anferov38@gmail.com>
2023-05-08 12:05:26 +0800

    Отключаем анонсы некротика от заражения

--- DBM-AntorusBurningThrone/Varimathras.lua
+++ DBM-AntorusBurningThrone/Varimathras.lua
@@ -297,7 +297,7 @@ function mod:SPELL_AURA_APPLIED(args)
 			if self.vb.totalEmbrace == 1 then --волосали2
 				--[[if DBM:GetRaidRank() > 0 and self:AntiSpam(2, "necrotic") then
 					prepareMessage(self, "premsg_Varimathras_necrotic_rw", nil, args.destName)
-				else]]if --[[DBM:GetRaidRank() == 0 and ]]self:AntiSpam(2, "necrotic") then
+				else]]if --[[DBM:GetRaidRank() == 0 and ]]self:AntiSpam(25, "necrotic") then
 					DBM:Debug(L.NecroticYell:format(args.destName, necrotic))
 				end
 			end
