f27a4d02f3a1aeab14bb6be1b90be0e11ddf0663
Arekusandoru <82957716+Aleksart163@users.noreply.github.com>
2023-06-17 11:03:54 +0400

    Update
    Новый спелл

--- DBM-AntorusBurningThrone/AntorusTrash.lua
+++ DBM-AntorusBurningThrone/AntorusTrash.lua
@@ -8,7 +8,7 @@ mod:SetUsedIcons(8, 7, 6, 5, 4, 3, 2, 1)
 mod.isTrashMod = true
 
 mod:RegisterEvents(
-	"SPELL_CAST_START 246209 245807 246444 254500 246505 245888",
+	"SPELL_CAST_START 246209 245807 246444 254500 246505 245888 246903",
 	"SPELL_CAST_SUCCESS 246664",
 	"SPELL_AURA_APPLIED 252760 246692 253600 254122 249297 246199 254948 246698 244399 254509 257920 248757 252797 245770 246687 254502",
 	"SPELL_AURA_APPLIED_DOSE 257920 248757",
@@ -32,6 +32,7 @@ local warnSoulburn						= mod:NewTargetAnnounce(253600, 3) --Горящая ду
 local specWarnPyroblast					= mod:NewSpecialWarningYou(246505, nil, nil, nil, 2, 6) --Огненная глыба
 local specWarnPyroblast2				= mod:NewSpecialWarningInterrupt(246505, "HasInterrupt", nil, nil, 1, 2) --Огненная глыба
 local specWarnShadowStorm				= mod:NewSpecialWarningInterrupt(245888, "HasInterrupt", nil, nil, 1, 2) --Буря Тени
+local specWarnDrawFlames				= mod:NewSpecialWarningInterrupt(246903, "HasInterrupt", nil, nil, 1, 2) --Призыв огней
 --Крушитель Кин'гарота
 local specWarnDecimation				= mod:NewSpecialWarningYouMoveAway(246687, nil, nil, nil, 4, 3) --Децимация
 local specWarnDecimation2				= mod:NewSpecialWarningDodge(246687, "-Tank", nil, nil, 2, 2) --Децимация
@@ -142,9 +143,11 @@ function mod:SPELL_CAST_START(args)
 	elseif spellId == 245888 and self:CheckInterruptFilter(args.sourceGUID, false, true) then --Буря Тени
 		specWarnShadowStorm:Show()
 		specWarnShadowStorm:Play("kickcast")
+	elseif spellId == 246903 and self:CheckInterruptFilter(args.sourceGUID, false, true) then --Призыв огней
+		specWarnDrawFlames:Show()
+		specWarnDrawFlames:Play("kickcast")
 	end
 end
-
 --[[
 function mod:SPELL_CAST_SUCCESS(args)
 	if not self.Options.Enabled then return end
