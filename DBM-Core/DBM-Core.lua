54a0cf4dbb8208b935d4ead08cbc2e12411359da
Arekusandoru <82957716+Aleksart163@users.noreply.github.com>
2023-06-25 01:04:22 +0400

    ап

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -34,9 +34,9 @@
 -------------------------------
 
 DBM = {
-	Revision = tonumber(("$Revision: 17721 $"):sub(12, -3)), --прошляпанное очко Мурчаля Прошляпенко [✔]
-	DisplayVersion = "7.3.45 Right Version " .. string.sub(GetLocale(), -2),
-	ReleaseRevision = 17720
+	Revision = tonumber(("$Revision: 17722 $"):sub(12, -3)), --прошляпанное очко Мурчаля Прошляпенко [✔]
+	DisplayVersion = "7.3.46 Right Version " .. string.sub(GetLocale(), -2),
+	ReleaseRevision = 17721
 }
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
 
