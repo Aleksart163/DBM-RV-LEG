3291c9527f8b74482bd62ecec6c776ed7718bece
Arekusandoru <82957716+Aleksart163@users.noreply.github.com>
2023-08-22 17:21:50 +0400

    update

--- DBM-Core/localization.de.lua
+++ DBM-Core/localization.de.lua
@@ -1,7 +1,7 @@
 if GetLocale() ~= "deDE" then return end
 
 DBM_HOW_TO_USE_MOD					= "Willkommen bei DBM. Tippe /dbm help für eine Liste unterstützter Kommandos. Für den Zugriff auf Einstellungen tippe /dbm in den Chat um die Konfiguration zu beginnen. Lade gewünschte Zonen manuell um jegliche Boss-spezifische Einstellungen nach deinen Wünschen zu konfigurieren. DBM setzt Standardeinstellungen für deine Spezialisierung, die du aber noch genauer anpassen kannst."
-DBM_FORUMS_MESSAGE					= "Haben Sie einen Fehler oder einen falschen Timer gefunden? Sind Sie der Meinung, dass ein Modul eine zusätzliche Warnung, einen Timer, ein Feature oder eine Lokalisierung benötigt? \nAlle Fragen können auf discord Aleksart163, /w Куплиняшка angesprochen werden oder hinterlassen Sie Anfragen auf |h|cff3588ffhttps://github.com/Aleksart163/DBM-RV|r"
+DBM_FORUMS_MESSAGE					= "Haben Sie einen Fehler oder einen falschen Timer gefunden? Sind Sie der Meinung, dass ein Modul eine zusätzliche Warnung, einen Timer, ein Feature oder eine Lokalisierung benötigt? \nAlle Fragen können auf discord Aleksart163, /w Куплиняшка angesprochen werden oder hinterlassen Sie Anfragen auf |h|cff3588ffhttps://github.com/Aleksart163/DBM-RV/pulls|r"
 DBM_SILENT_REMINDER					= "Erinnerung: DBM befindet sich noch im Lautlos-Modus."
 
 DBM_CORE_LOAD_MOD_ERROR				= "Fehler beim Laden der Boss Mods für %s: %s"
