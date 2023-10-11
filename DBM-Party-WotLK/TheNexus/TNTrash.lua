a04964683dfb84f26bd72f84623a8089b3111107
Arekusandoru <82957716+Aleksart163@users.noreply.github.com>
2023-10-11 07:38:29 +0400

    Update

--- DBM-Party-WotLK/TheNexus/TNTrash.lua
+++ DBM-Party-WotLK/TheNexus/TNTrash.lua
@@ -7,6 +7,7 @@ mod:SetZone()
 mod.isTrashMod = true
 
 mod:RegisterEvents(
+	"SPELL_CAST_START 13323",
 	"SPELL_AURA_APPLIED 13323",
 	"SPELL_AURA_REMOVED 13323"
 )
@@ -14,8 +15,19 @@ mod:RegisterEvents(
 --Нексус треш https://www.wowhead.com/ru/zone=4265/нексус#npcs
 local warnPolymorph						= mod:NewTargetAnnounce(13323, 2) --Превращение
 
+local specWarnPolymorph					= mod:NewSpecialWarningInterrupt(13323, "HasInterrupt", nil, nil, 1, 2) --Превращение
+
 local yellPolymorph						= mod:NewYellDispel(13323, nil, nil, nil, "YELL") --Превращение
 local yellPolymorph2					= mod:NewFadesYell(13323, nil, nil, nil, "YELL") --Превращение
+
+function mod:SPELL_CAST_START(args)
+	if not self.Options.Enabled then return end
+	local spellId = args.spellId
+	if spellId == 13323 and self:CheckInterruptFilter(args.sourceGUID, false, true) then --Превращение
+		specWarnPolymorph:Show()
+		specWarnPolymorph:Play("kickcast")
+	end
+end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if not self.Options.Enabled then return end
