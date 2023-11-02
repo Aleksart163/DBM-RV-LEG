a00382e4db76af76c3e55ff7151c349644debc26
Arekusandoru <82957716+Aleksart163@users.noreply.github.com>
2023-11-02 16:48:28 +0400

    Update Proshlyapation of Murchal

--- DBM-WorldEvents/Holidays/Ravenn.lua
+++ DBM-WorldEvents/Holidays/Ravenn.lua
@@ -10,27 +10,32 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START 183224 67729",
-	"SPELL_AURA_APPLIED 28467",
+	"SPELL_AURA_APPLIED 28467 154769",
 	"SPELL_AURA_APPLIED_DOSE 28467",
-	"SPELL_DAMAGE 67781",
-	"SPELL_MISSED 67781",
-	"SPELL_ABSORBED 67781",
+	"SPELL_DAMAGE 93691",
+	"SPELL_MISSED 93691",
+	"SPELL_ABSORBED 93691",
 	"CHAT_MSG_MONSTER_YELL"
 )
 
+------------------------------------------
+-- Всё прошляпанно Мурчалем Прошляпенко --
+------------------------------------------
 local warnExplode						= mod:NewCastAnnounce(67729, 4, nil, nil, "Ranged") --Взрыв
 local warnShadowBoltVolley				= mod:NewCastAnnounce(183224, 2) --Залп стрел Тьмы
 local warnMortalWound					= mod:NewStackAnnounce(28467, 2, nil, "Tank|Healer") --Смертельная рана
+local warnFixate						= mod:NewTargetAnnounce(154769, 2) --Сосредоточение внимания
 
+local specWarnFixate					= mod:NewSpecialWarningYouRun(154769, nil, nil, nil, 4, 3) --Сосредоточение внимания
 local specWarnMortalWound				= mod:NewSpecialWarningStack(28467, nil, 5, nil, nil, 3, 6) --Смертельная рана
 local specWarnMortalWound2				= mod:NewSpecialWarningTaunt(28467, nil, nil, nil, 3, 6) --Рассечение
 local specWarnExplode					= mod:NewSpecialWarningDodge(67729, "Melee", nil, nil, 2, 3) --Взрыв
-local specWarnDesecration				= mod:NewSpecialWarningYouMove(67781, nil, nil, nil, 1, 2) --Осквернение
+local specWarnDesecration				= mod:NewSpecialWarningYouMove(93691, nil, nil, nil, 1, 2) --Осквернение
 local specWarnSummonDrudgeGhouls		= mod:NewSpecialWarningSwitch(70358, "Tank|Dps", nil, nil, 1, 2) --Призыв вурдалаков
 
 local timerShadowBoltVolleyCD			= mod:NewCDTimer(12.3, 183224, nil, nil, nil, 2, nil, DBM_CORE_HEALER_ICON) --Залп стрел Тьмы
-local timerSummonDrudgeGhoulsCD			= mod:NewCDTimer(30, 70358, nil, nil, nil, 1, nil, DBM_CORE_TANK_ICON..DBM_CORE_DAMAGE_ICON) --Призыв вурдалаков
-local timerPumpkinCD					= mod:NewTimer(35, "MurchalProshlyapTimer", "Interface\\Icons\\Inv_misc_bag_28_halloween", nil, nil, 1, DBM_CORE_DAMAGE_ICON) --призыв тыкв
+local timerSummonDrudgeGhoulsCD			= mod:NewCDTimer(38, 70358, nil, nil, nil, 1, nil, DBM_CORE_TANK_ICON..DBM_CORE_DAMAGE_ICON) --Призыв вурдалаков
+local timerPumpkinCD					= mod:NewTimer(32, "MurchalProshlyapTimer", "Interface\\Icons\\Inv_misc_bag_28_halloween", nil, nil, 1, DBM_CORE_DAMAGE_ICON) --призыв тыкв
 
 function mod:OnCombatStart(delay)
 --	timerShadowBoltVolleyCD:Start(12.5)
@@ -73,12 +78,19 @@ function mod:SPELL_AURA_APPLIED(args)
 		else
 			warnMortalWound:Show(args.destName, amount)
 		end
+	elseif spellId == 154769 then --Сосредоточение внимания
+		if args:IsPlayer() then
+			specWarnFixate:Show()
+			specWarnFixate:Play("justrun")
+		else
+			warnFixate:CombinedShow(0.5, args.destName)
+		end
 	end
 end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:SPELL_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId, spellName)
-	if spellId == 67781 and destGUID == UnitGUID("player") and self:AntiSpam(3, "Desecration") then --Осквернение
+	if spellId == 93691 and destGUID == UnitGUID("player") and self:AntiSpam(3, "Desecration") then --Осквернение
 		specWarnDesecration:Show()
 		specWarnDesecration:Play("runout")
 	end
@@ -100,7 +112,7 @@ function mod:OnSync(msg)
 			specWarnSummonDrudgeGhouls:Show()
 			specWarnSummonDrudgeGhouls:Play("mobkill")
 		end
-		timerSummonDrudgeGhoulsCD:Start(42)
+		timerSummonDrudgeGhoulsCD:Start()
 	elseif msg == "MurchalProshlyapation2" then
 		timerPumpkinCD:Start()
 	end
