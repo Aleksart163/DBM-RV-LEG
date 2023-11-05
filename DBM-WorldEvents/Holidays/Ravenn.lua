199af7db4b81389129b93a7468ec96d591000ebb
Arekusandoru <82957716+Aleksart163@users.noreply.github.com>
2023-11-05 21:13:45 +0400

    fix and update

--- DBM-WorldEvents/Holidays/Ravenn.lua
+++ DBM-WorldEvents/Holidays/Ravenn.lua
@@ -16,6 +16,7 @@ mod:RegisterEventsInCombat(
 	"SPELL_DAMAGE 93691",
 	"SPELL_MISSED 93691",
 	"SPELL_ABSORBED 93691",
+	"UNIT_AURA player",
 	"CHAT_MSG_MONSTER_YELL"
 )
 
@@ -23,12 +24,11 @@ mod:RegisterEventsInCombat(
 -- Всё прошляпанно Мурчалем Прошляпенко --
 ------------------------------------------
 local warnExplode						= mod:NewCastAnnounce(67729, 4, nil, nil, "Ranged") --Взрыв
---local warnShadowBoltVolley				= mod:NewCastAnnounce(183224, 2) --Залп стрел Тьмы
 local warnMortalWound					= mod:NewStackAnnounce(28467, 2, nil, "Tank|Healer") --Смертельная рана
-local warnFixate						= mod:NewTargetAnnounce(154769, 2) --Сосредоточение внимания
+--local warnFixate						= mod:NewTargetAnnounce(154769, 2) --Сосредоточение внимания
 
-local specWarnFixate					= mod:NewSpecialWarningYouRun(154769, nil, nil, nil, 4, 3) --Сосредоточение внимания
-local specWarnFixate2					= mod:NewSpecialWarningEnd(154769, nil, nil, nil, 1, 2) --Сосредоточение внимания
+local specWarnFixate					= mod:NewSpecialWarningYouRun(310042, nil, nil, nil, 4, 3) --Сосредоточение внимания
+local specWarnFixate2					= mod:NewSpecialWarningEnd(310042, nil, nil, nil, 1, 2) --Сосредоточение внимания
 local specWarnMortalWound				= mod:NewSpecialWarningStack(28467, nil, 5, nil, nil, 3, 6) --Смертельная рана
 local specWarnMortalWound2				= mod:NewSpecialWarningTaunt(28467, nil, nil, nil, 3, 6) --Рассечение
 local specWarnExplode					= mod:NewSpecialWarningDodge(67729, "Melee", nil, nil, 2, 3) --Взрыв
@@ -41,9 +41,13 @@ local timerPumpkinCD					= mod:NewTimer(32, "MurchalProshlyapTimer", "Interface\
 
 local yellFixate						= mod:NewYell(244653, nil, nil, nil, "YELL") --Сосредоточение внимания
 
-mod:AddSetIconOption("SetIconOnFixate", 154769, true, false, {4, 3, 2, 1})
+local countdownMurchalShlyap			= mod:NewCountdown("Alt32", 70358, nil, nil, 5)
+
+--mod:AddSetIconOption("SetIconOnFixate", 154769, true, false, {4, 3, 2, 1})
 
 mod.vb.MurchalItsOchkenProshlyapation = 0
+local proshlyapationOfMurchal = DBM:GetSpellInfo(310042)
+local murchalProshlyaping = false
 
 function mod:OnCombatStart(delay)
 	self.vb.MurchalItsOchkenProshlyapation = 0
@@ -54,7 +58,6 @@ end
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
 	if spellId == 183224 then --Залп стрел Тьмы
-	--	warnShadowBoltVolley:Show()
 		timerShadowBoltVolleyCD:Start()
 	elseif spellId == 67729 and self:AntiSpam(2, "Explode") then --Взрыв
 		warnExplode:Show()
@@ -87,7 +90,7 @@ function mod:SPELL_AURA_APPLIED(args)
 		else
 			warnMortalWound:Show(args.destName, amount)
 		end
-	elseif spellId == 154769 then --Сосредоточение внимания
+--[[	elseif spellId == 154769 then --Сосредоточение внимания
 		self.vb.MurchalItsOchkenProshlyapation = self.vb.MurchalItsOchkenProshlyapation + 1
 		if args:IsPlayer() then
 			specWarnFixate:Show()
@@ -99,11 +102,12 @@ function mod:SPELL_AURA_APPLIED(args)
 		end
 		if self.Options.SetIconOnFixate then
 			self:SetIcon(args.destName, self.vb.MurchalItsOchkenProshlyapation)
-		end
+		end]]
 	end
 end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
+--[[
 function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
 	if spellId == 154769 then --Сосредоточение внимания
@@ -116,10 +120,10 @@ function mod:SPELL_AURA_REMOVED(args)
 			self:RemoveIcon(args.destName)
 		end
 	end
-end
+end]]
 
 function mod:SPELL_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId, spellName)
-	if spellId == 93691 and destGUID == UnitGUID("player") and self:AntiSpam(3, "Desecration") then --Осквернение
+	if spellId == 93691 and destGUID == UnitGUID("player") and self:AntiSpam(2, "Desecration") then --Осквернение
 		specWarnDesecration:Show()
 		specWarnDesecration:Play("runout")
 	end
@@ -132,6 +136,21 @@ function mod:CHAT_MSG_MONSTER_YELL(msg)
 		self:SendSync("MurchalProshlyapation")
 	elseif msg == L.MurchalProshlyap3 then
 		self:SendSync("MurchalProshlyapation2")
+	end
+end
+
+do
+	function mod:UNIT_AURA(uId)
+		local proshlyap = UnitDebuff("player", proshlyapationOfMurchal)
+		if proshlyap and not murchalProshlyaping then
+			murchalProshlyaping = true
+			specWarnFixate:Show()
+			specWarnFixate:Play("justrun")
+			specWarnFixate:ScheduleVoice(2, "keepmove")
+			yellFixate:Yell()
+		elseif not proshlyap and murchalProshlyaping then
+			murchalProshlyaping = false
+		end
 	end
 end
 
