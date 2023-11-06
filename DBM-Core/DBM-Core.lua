abb2428738e8c3536e47154636364ee245c64057
DanzoGit <anferov38@gmail.com>
2023-11-07 01:52:40 +0800

    fix and up

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -34,9 +34,9 @@
 -------------------------------
 
 DBM = {
-	Revision = tonumber(("$Revision: 17741 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 17742 $"):sub(12, -3)),
 	DisplayVersion = "7.3.49 Right Version " .. string.sub(GetLocale(), -2),
-	ReleaseRevision = 17740
+	ReleaseRevision = 17741
 }
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
 
